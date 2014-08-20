30b9ea4d011ce923a4cc59033bb1b3169528371c
Patrick <Patrick.David3@gmail.com>
2014-08-20 22:37:17 +0000

    add a stop to the stops table in the test trains database

--- lib/stops.rb
+++ lib/stops.rb
@@ -7,4 +7,26 @@ class Stops
     @line_id = attributes['line_id']
     @id = attributes['id']
   end
+  
+  def self.all
+    results = DB.exec("SELECT * FROM stops;")
+    stops = []
+    results.each do |result|
+      puts result    
+      id = result['id'].to_i
+      station_id = result['stations_id'].to_i
+      line_id = result['lines_id'].to_i
+      stops << Stops.new({'id' => id, 'station_id' => station_id, 'line_id' => line_id})
+    end
+    stops
+  end
+  
+  def ==(another_stop)
+    self.station_id == another_stop.station_id && self.line_id == another_stop.line_id
+  end
+  
+   def save
+    results = DB.exec("INSERT INTO stops (stations_id, lines_id) VALUES (#{@station_id}, #{@line_id}) RETURNING id;")
+    @id = results.first['id'].to_i
+  end
 end
--- spec/stops_spec.rb
+++ spec/stops_spec.rb
@@ -3,8 +3,20 @@ require 'spec_helper'
 describe 'Stops' do
   it 'instantiates a stop with id, line_id and station_id' do
     test_line = Lines.new({'name' => "Orange"})
+    test_line.save
     test_station = Stations.new({'name' => "Vienna/GMU"})
-    test_stop = Stops.new({'line_id' => test_line.id, 'station_id' => test_station.id})
+    test_station.save
+    test_stop = Stops.new({'station_id' => test_station.id, 'line_id' => test_line.id})
     expect(test_stop).to be_an_instance_of Stops
+  end
+  
+  it 'saves stops into our stop database' do
+    test_line = Lines.new({'name' => "Orange"})
+    test_line.save
+    test_station = Stations.new({'name' => "Vienna/GMU"})
+    test_station.save
+    test_stop = Stops.new({'station_id' => test_station.id, 'line_id' => test_line.id})
+    test_stop.save
+    expect(Stops.all).to eq [test_stop]
   end
 end
