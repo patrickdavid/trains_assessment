088e6dbd6c6d63cdf8680aedb01c1969fe8f5662
Patrick <Patrick.David3@gmail.com>
2014-08-20 17:32:18 +0000

    save a station to the stations table in the trains database

--- lib/stations.rb
+++ lib/stations.rb
@@ -6,4 +6,25 @@ class Stations
     @name = attributes['name']
     @id = attributes['id']
   end
+  
+  def self.all
+    results = DB.exec("SELECT * FROM stations;")
+    @stations = []
+    results.each do |result|
+      name = result['name']
+      id = result['id'].to_i
+      @stations << Stations.new({'name' => name, 'id' => id})
+    end
+    @stations
+  end
+  
+  def save
+    @id = DB.exec("INSERT INTO stations (name) VALUES ('#{@name}') RETURNING id;").first['id'].to_i
+  end
+  
+  def ==(another_station)
+    self.name == another_station.name
+  end
+  
+  
 end
--- spec/stations_spec.rb
+++ spec/stations_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe 'Stations' do
   it 'Initializes the Stations class' do
-    test_station = Stations.new({:name => "Vienna/GMU"})
+    test_station = Stations.new({'name' => "Vienna/GMU"})
     expect(test_station).to be_an_instance_of Stations
+  end
+  
+  it 'saves stations into our stations database' do
+    test_station = Stations.new({'name' => "Vienna/GMU"})
+    test_station.save
+    expect(Stations.all).to eq [test_station]
   end
 end
