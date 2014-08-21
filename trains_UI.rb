95d5669a44b960624ce5ba9cdbb382c1b3c33837
Patrick <Patrick.David3@gmail.com>
2014-08-21 18:48:38 +0000

    allow users to delete a line or station via the UI

--- trains_UI.rb
+++ trains_UI.rb
@@ -31,8 +31,10 @@ def operator_menu
   loop do
     puts "Press 'l' to list all lines"
     puts "Press 's' to list all stations"
-    puts "Press 'al' to add line to station"
-    puts "Press 'as' to add a staion to a line"
+    puts "Press 'al' to add a line"
+    puts "Press 'as' to add a station"
+    puts "Press 'rl' to remove a line"
+    puts "Press 'rs' to remove a station"
     puts "Press 'ls' to list all stations of a specific line"
     puts "Press 'll' to list all lines of a specific station"
     puts "Press 'ns' to change a name of a station"
@@ -49,6 +51,10 @@ def operator_menu
       add_line
     elsif menu_choice == 'as'
       add_station
+    elsif menu_choice == 'rl'
+      remove_line
+    elsif menu_choice == 'rs'
+      remove_station
     elsif menu_choice == 'ls'
       list_stations_of_line
     elsif menu_choice == 'll'
@@ -91,5 +97,52 @@ def commuter_menu
     end
   end
 end
+
+def list_lines
+  Lines.all.each do |line|
+    puts line.name
+  end
+end
+
+def list_stations
+  Stations.all.each do |station|
+    puts station.name
+  end
+end
+
+def add_station
+  puts "Please enter the name of the station."
+  name = gets.chomp
+  new_station = Stations.new({'name' => name})
+  new_station.save
+  puts "The station has been added."
+end
+
+def add_line
+  puts "Please enter the name of the line."
+  name = gets.chomp
+  new_line = Lines.new({'name' => name})
+  new_line.save
+  puts "The line has been added."
+end
+
+def remove_line
+  
+end
+
+def remove_station
+  puts "Which station would you like to remove"
+  input_station = gets.chomp
+  Stations.delete(input_station)
+  puts "Station removed"
+end
+
+def remove_line
+  puts "Which line would you like to remove"
+  input_line = gets.chomp
+  Lines.delete(input_line)
+  puts "Line removed"
+end
+
 
 main_menu
