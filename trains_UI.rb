81dcbf542bbd80704275793a92eb1b79ce834897
Patrick <Patrick.David3@gmail.com>
2014-08-23 15:52:47 +0000

    allow the user through the UI to list stations for particular lines and vice versa

--- trains_UI.rb
+++ trains_UI.rb
@@ -31,14 +31,13 @@ def operator_menu
   loop do
     puts "Press 'l' to list all lines"
     puts "Press 's' to list all stations"
+    puts "press 'ss' to add a stop"
     puts "Press 'al' to add a line"
     puts "Press 'as' to add a station"
     puts "Press 'rl' to remove a line"
     puts "Press 'rs' to remove a station"
     puts "Press 'ls' to list all stations of a specific line"
     puts "Press 'll' to list all lines of a specific station"
-    puts "Press 'ns' to change a name of a station"
-    puts "Press 'nl' to change a name of a line"
     puts "Press 'm' to return to main menu"
 
     menu_choice = gets.chomp.downcase
@@ -47,6 +46,8 @@ def operator_menu
       list_lines
     elsif menu_choice == 's'
       list_stations
+    elsif menu_choice == 'ss'
+      add_stop
     elsif menu_choice == 'al'
       add_line
     elsif menu_choice == 'as'
@@ -59,10 +60,6 @@ def operator_menu
       list_stations_of_line
     elsif menu_choice == 'll'
       list_lines_of_station
-    elsif menu_choice == 'ns'
-      new_station_name
-    elsif menu_choice == 'nl'
-      new_line_name
     elsif menu_choice == 'm'
       main_menu
     else
@@ -126,8 +123,22 @@ def add_line
   puts "The line has been added."
 end
 
-def remove_line
-  
+def add_stop
+  puts "Please enter the name of the station."
+  station_name = gets.chomp
+  new_station = Stations.new({'name' => station_name})
+  new_station.save
+  puts "The station has been added."
+  puts "\n\n\n\n"
+  puts "Please enter the name of the line."
+  line_name = gets.chomp
+  new_line = Lines.new({'name' => line_name})
+  new_line.save
+  puts "The line has been added."
+  new_stop = Stops.new({'station_id' => new_station.id, 'line_id' => new_line.id})
+  new_stop.save
+  puts "\n\n\n"
+  puts "The stop has been saved"
 end
 
 def remove_station
@@ -142,6 +153,39 @@ def remove_line
   input_line = gets.chomp
   Lines.delete(input_line)
   puts "Line removed"
+end
+
+def list_stations_of_line
+  puts "Which line would you like to see the stations for?"
+  result = ''
+  input_line = gets.chomp
+  puts "Here are all the stations for that particular line"
+  Lines.all.each do |line|
+    if input_line == line.name
+      result = line
+    end
+    stats = result.stations
+    stats.each do |stat|
+      puts stat.name
+    end
+  end
+end
+
+def list_lines_of_station
+  puts "Enter the station name to see which lines stop here."
+  result = ''
+  result1 = ''
+  input_station = gets.chomp
+  puts "Here are all the lines for that particular station"
+  Stations.all.each do |station|
+    if input_station == station.name
+      result = station
+    end
+    line = result.lines
+    line.each do |lin|
+      puts lin.name
+    end
+  end
 end
 
 
